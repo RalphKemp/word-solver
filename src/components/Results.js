2d40976861e19b967ac2bdf51c56732d62996e68
ralph kemp <ralphk3mp@gmail.com>
2019-01-05 00:23:27 +0000

    deleted boilerplate code, created new basic components, hooking up redux store so redux form will work, added styled components

---
+++ src/components/Results.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MainResultsDiv = styled.div`
+  height: 100vh;
+  width: 50vw;
+  background-color: blue;
+  color: white;
+`;
+
+const Results = () => {
+  return (
+    <MainResultsDiv>
+      Results
+    </MainResultsDiv>
+  );
+}
+
+export default Results;
