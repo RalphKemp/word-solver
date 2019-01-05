b57332f68673dbfbc9030088f351deae9be1eeb2
ralph kemp <ralphk3mp@gmail.com>
2019-01-05 01:49:51 +0000

    fixed api call by passing it thorugh a heroku project lol, fixed up redux form, rendering words on results page, added uuidv1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
-import axios from 'axios';
-import { FETCH_WORDS } from './types';
+import axios from "axios";
+import { FETCH_WORDS } from "./types";
 
+export const fetchWords = word => async dispatch => {
+  const res = await axios.get(
+    `${'https://cors-anywhere.herokuapp.com/'}https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/regions%3Dbritish%2C?word_length=4%2C&exact=false&limit=10`,
+    {
+      headers: {
+        'Accept': "application/json",
+        'app_id': "4bb65adf",
+        'app_key': "f0952a9ea06c8a6b1fc1bbb40a2e5688"
+      }
+    }
+  );
+  dispatch({ type: FETCH_WORDS, payload: res.data });
+};
